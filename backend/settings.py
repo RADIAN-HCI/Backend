507f738c62374b270098236572090d405ba776de
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2024-06-06 14:36:56 +0330

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure--a&low2axj895%4ec5z@bwuips6q&xhopuw0#7re1l07o#03_o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "testserver"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "testserver", "http://82.115.20.169", "82.115.20.169",]
 
 
 # Application definition
