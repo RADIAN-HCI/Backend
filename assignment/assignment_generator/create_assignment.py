210184165fc3903a3d7ff258aeec6387f7dc207c
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2024-06-07 16:26:06 +0330

    Update create_assignment.py

--- assignment/assignment_generator/create_assignment.py
+++ assignment/assignment_generator/create_assignment.py
@@ -5,109 +5,112 @@ from assignment.models import Assignment
 
 
 def generate_assignment_pdf(assignment, questions, university_name, university_logo):
-    # Create a new document
-    # doc = Document()
-    doc = Document(geometry_options={"margin": "2cm"})
-    doc.packages.append(pylatex.Package("graphicx"))
-
-    # Add header with university logo, name, professor, etc.
-    # with doc.create(Section('')):
-    # doc.append(NoEscape(r'\hrule'))
-    # doc.append(NoEscape(r'\begin{center}'))
-    # # Include university logo
-    # doc.append(NoEscape(r'\includegraphics[width=2cm]{%s}' % university_logo))
-    # doc.append(NoEscape(r'\vspace{0.5cm}'))  # Adjust vertical space
-    # # University name
-    # doc.append(NoEscape(r'\textbf{\LARGE %s}' % university_name))
-    # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
-    # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
-    # # Professor name
-    # doc.append(NoEscape(r'\textbf{Professor:} %s' % professor_name))
-    # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
-    # # Course details
-    # doc.append(NoEscape(r'\textbf{Course:} %s' % assignment.course.name))
-    # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
-    # # Assignment type and title
-    # doc.append(NoEscape(r'\textbf{Assignment Type:} %s' % assignment.assignment_type))
-    # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
-    # doc.append(NoEscape(r'\textbf{Title:} %s' % assignment.title))
-    # doc.append(NoEscape(r'\vspace{0.5cm}'))
-    # doc.append(NoEscape(r'\hrule'))
-    # doc.append(NoEscape(r'\end{center}'))
-
-    with doc.create(MiniPage(width=r"0.6\linewidth")):
-        doc.append(NoEscape(r"\begin{flushleft}"))
-        # Left Column: Course name, assignment type, professor name
-        doc.append(NoEscape(r"\large{Course: \textbf{%s}}" % assignment.course.name))
-        doc.append(Command("vspace", "0.1cm"))
-        doc.append(Command("newline"))
-        doc.append(Command("vspace", "0.1cm"))
-        # doc.append(Command('vspace', '0.3cm'))
-        doc.append(
-            NoEscape(
-                r"\normalsize{Professor: \textbf{%s}}"
-                % assignment.course.professor_name
-            )
-        )
-        doc.append(Command("newline"))
-        doc.append(Command("vspace", "0.1cm"))
-        # doc.append(Command('vspace', '0.3cm'))
-        doc.append(
-            NoEscape(
-                r"\normalsize{\textbf{%s} assignment}" % assignment.assignment_type
-            )
-        )
-        doc.append(NoEscape(r"\end{flushleft}"))
-
-    with doc.create(MiniPage(width=r"0.36\linewidth")):
-        doc.append(NoEscape(r"\begin{flushright}"))
-        doc.append(
-            Command(
-                "includegraphics", options="width=1.75cm", arguments=university_logo
-            )
-        )
-        doc.append(
-            NoEscape(r"\hfill")
-        )  # Add horizontal space to push content to the right
-        doc.append(Command("newline"))
-        doc.append(NoEscape(r"\hfill"))
-        doc.append(
-            Command(
-                "textbf", arguments=Command("normalsize", arguments=university_name)
-            )
-        )
-        doc.append(NoEscape(r"\end{flushright}"))
-
-    doc.append(NoEscape(r"\begin{center}"))
-    doc.append(NoEscape(r"\hrule height 0.02cm"))
-    doc.append(Command("vspace", "0.2cm"))
-    doc.append(NoEscape(r"\large{\textbf{%s}}" % assignment.title))
-    doc.append(Command("vspace", "0.2cm"))
-    doc.append(NoEscape(r"\hrule height 0.02cm"))
-    doc.append(NoEscape(r"\end{center}"))
-
-    # Add questions
-    for idx, question in enumerate(
-        questions.filter(is_selected_for_assignment=True).order_by("order"), start=1
-    ):
-        with doc.create(Section(NoEscape(r"\textbf{%s}" % question.title))):
-            # Question details
-            doc.append(NoEscape(r"%s" % question.details_modified))
-            # Add attachment if available
-            if question.attachment:
-                doc.append(NoEscape(r"\begin{center}"))
-                doc.append(
-                    Command(
-                        "includegraphics",
-                        options="width=8cm",
-                        arguments=question.attachment.url.replace("/latex/", "./"),
-                    )
+    try:
+        # Create a new document
+        # doc = Document()
+        doc = Document(geometry_options={"margin": "2cm"})
+        doc.packages.append(pylatex.Package("graphicx"))
+    
+        # Add header with university logo, name, professor, etc.
+        # with doc.create(Section('')):
+        # doc.append(NoEscape(r'\hrule'))
+        # doc.append(NoEscape(r'\begin{center}'))
+        # # Include university logo
+        # doc.append(NoEscape(r'\includegraphics[width=2cm]{%s}' % university_logo))
+        # doc.append(NoEscape(r'\vspace{0.5cm}'))  # Adjust vertical space
+        # # University name
+        # doc.append(NoEscape(r'\textbf{\LARGE %s}' % university_name))
+        # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
+        # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
+        # # Professor name
+        # doc.append(NoEscape(r'\textbf{Professor:} %s' % professor_name))
+        # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
+        # # Course details
+        # doc.append(NoEscape(r'\textbf{Course:} %s' % assignment.course.name))
+        # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
+        # # Assignment type and title
+        # doc.append(NoEscape(r'\textbf{Assignment Type:} %s' % assignment.assignment_type))
+        # doc.append(NoEscape(r'\vspace{0.3cm}'))  # Adjust vertical space
+        # doc.append(NoEscape(r'\textbf{Title:} %s' % assignment.title))
+        # doc.append(NoEscape(r'\vspace{0.5cm}'))
+        # doc.append(NoEscape(r'\hrule'))
+        # doc.append(NoEscape(r'\end{center}'))
+    
+        with doc.create(MiniPage(width=r"0.6\linewidth")):
+            doc.append(NoEscape(r"\begin{flushleft}"))
+            # Left Column: Course name, assignment type, professor name
+            doc.append(NoEscape(r"\large{Course: \textbf{%s}}" % assignment.course.name))
+            doc.append(Command("vspace", "0.1cm"))
+            doc.append(Command("newline"))
+            doc.append(Command("vspace", "0.1cm"))
+            # doc.append(Command('vspace', '0.3cm'))
+            doc.append(
+                NoEscape(
+                    r"\normalsize{Professor: \textbf{%s}}"
+                    % assignment.course.professor_name
                 )
-                doc.append(NoEscape(r"\end{center}"))
-                # doc.append(NoEscape(r'\textbf{Attachment:} %s' % question.attachment.url))
-
-    # Generate PDF
-    doc.generate_pdf("latex/assignment_pdf", clean_tex=False)
+            )
+            doc.append(Command("newline"))
+            doc.append(Command("vspace", "0.1cm"))
+            # doc.append(Command('vspace', '0.3cm'))
+            doc.append(
+                NoEscape(
+                    r"\normalsize{\textbf{%s} assignment}" % assignment.assignment_type
+                )
+            )
+            doc.append(NoEscape(r"\end{flushleft}"))
+    
+        with doc.create(MiniPage(width=r"0.36\linewidth")):
+            doc.append(NoEscape(r"\begin{flushright}"))
+            doc.append(
+                Command(
+                    "includegraphics", options="width=1.75cm", arguments=university_logo
+                )
+            )
+            doc.append(
+                NoEscape(r"\hfill")
+            )  # Add horizontal space to push content to the right
+            doc.append(Command("newline"))
+            doc.append(NoEscape(r"\hfill"))
+            doc.append(
+                Command(
+                    "textbf", arguments=Command("normalsize", arguments=university_name)
+                )
+            )
+            doc.append(NoEscape(r"\end{flushright}"))
+    
+        doc.append(NoEscape(r"\begin{center}"))
+        doc.append(NoEscape(r"\hrule height 0.02cm"))
+        doc.append(Command("vspace", "0.2cm"))
+        doc.append(NoEscape(r"\large{\textbf{%s}}" % assignment.title))
+        doc.append(Command("vspace", "0.2cm"))
+        doc.append(NoEscape(r"\hrule height 0.02cm"))
+        doc.append(NoEscape(r"\end{center}"))
+    
+        # Add questions
+        for idx, question in enumerate(
+            questions.filter(is_selected_for_assignment=True).order_by("order"), start=1
+        ):
+            with doc.create(Section(NoEscape(r"\textbf{%s}" % question.title))):
+                # Question details
+                doc.append(NoEscape(r"%s" % question.details_modified))
+                # Add attachment if available
+                if question.attachment:
+                    doc.append(NoEscape(r"\begin{center}"))
+                    doc.append(
+                        Command(
+                            "includegraphics",
+                            options="width=8cm",
+                            arguments=question.attachment.url.replace("/latex/", "./"),
+                        )
+                    )
+                    doc.append(NoEscape(r"\end{center}"))
+                    # doc.append(NoEscape(r'\textbf{Attachment:} %s' % question.attachment.url))
+    
+        # Generate PDF
+        doc.generate_pdf("latex/assignment_pdf", clean_tex=False)
+    except Exception as e:
+        print("An error occurred:", e)
 
     return doc.dumps()
 
